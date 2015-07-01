28b6aaeba089a086682860d1f0925118a3a0c482
Chris Holden <ceholden@gmail.com>
2015-07-01 00:54:24 -0400

    Force qt4 backend

--- src/plot.py
+++ src/plot.py
@@ -2,18 +2,26 @@
 """
 import os
 
+from matplotlib import use as mpl_use
+mpl_use('Qt4Agg')
 import matplotlib as mpl
 from matplotlib.backends.backend_qt4agg \
     import FigureCanvasQTAgg as FigureCanvas
 
+mpl_version = map(int, mpl.__version__.split('.'))
+
 
 # Note: FigureCanvas is also a QWidget
 class ROIPlot(FigureCanvas):
+
     """ TODO
     """
+
     def __init__(self):
         style = os.environ.get('ROITOOL_PLOT_STYLE', 'ggplot')
-        if style:
+
+        if style and mpl_version[0] >= 1 and mpl_version[1] >= 4:
+            import matplotlib.style
             if style == 'xkcd':
                 mpl.pyplot.xkcd()
             else:
