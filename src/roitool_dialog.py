bdc21650374d04a1f2371548040e6eeb69ead23b
Chris Holden <ceholden@gmail.com>
2015-07-01 00:54:51 -0400

    Wired most of GUI -- see TODOs in file

--- src/roitool_dialog.py
+++ src/roitool_dialog.py
@@ -20,9 +20,14 @@
  *                                                                         *
  ***************************************************************************/
 """
+from collections import OrderedDict
+from functools import partial
 import logging
 
-from PyQt4 import QtGui
+from PyQt4 import QtCore, QtGui
+
+from qgis.core import (QgsMapLayer, QgsRasterLayer, QgsVectorLayer,
+                       QgsMapLayerRegistry)
 
 from ui_roitool_dialog import Ui_ROIToolDialog
 
@@ -33,19 +38,223 @@ logger = logging.getLogger('roitool')
 
 
 class ROIToolDialog(QtGui.QDialog, Ui_ROIToolDialog):
+
     """ TODO
     """
+    layer_dict = dict(name=None, id=None, obj=None)
+    rlayers = OrderedDict()  # track raster layers as rlayer.id(): layer_dict
+    vlayers = OrderedDict()  # track vector layers as vlayer.id(): layer_dict
+
     def __init__(self, iface, parent=None):
         super(ROIToolDialog, self).__init__(parent)
         self.setupUi(self)
         self.iface = iface
 
+        self._init_gui()
         self._init_plot()
 
+    def _init_gui(self):
+        """ Initialize GUI components """
+        # Populate QComboBox with raster and vector layers
+        # TODO: I don't think we need to do this -- layersAdded does it
+        #       Should make sure it actually works 100% of time
+        #       RE: what if we enable plugin after layers added...
+        #           we need to do it
+        layers = QgsMapLayerRegistry.instance().mapLayers()
+        if layers:
+            self._map_layers_added(layers)
+        # Wire map layer added/remove events
+        QgsMapLayerRegistry.instance().layersAdded.connect(
+            self._map_layers_added)
+        QgsMapLayerRegistry.instance().layersWillBeRemoved.connect(
+            self._map_layers_removed)
+
+        # Wire vector QComboBox
+        self.combox_vector.currentIndexChanged.connect(self._vlayer_changed)
+
+        # TODO: set and wire vector layer field QComboBox
+        self.combox_field.clear()
+        idx = self.combox_vector.currentIndex()
+        self._vlayer_changed(idx)
+
+        # Wire buttons
+        self.but_update.clicked.connect(self._update_plot)
+        self.but_saveplot.clicked.connect(self._save_plot)
+        self.but_savestats.clicked.connect(self._export_data)
+
     def _init_plot(self):
+        """ Setup plot """
         self.plot = ROIPlot()
         self.widget_plot.setLayout(QtGui.QVBoxLayout())
         self.widget_plot.layout().addWidget(self.plot)
 
+# SIGNALS
+    @QtCore.pyqtSlot(list)
+    def _map_layers_added(self, layers):
+        """ Keep track of newly added layers
+
+        Adds new layers to either `self.rlayers` or `self.vlayers`.
+        Adds new layers to appropriate QComboBox
+        Wires QgsMapLayer rename (`layerNameChanged`) and data change signals
+          (dataChanged)
+
+        Args:
+          layers (list): list of Qgs[Raster|Vector]Layer
+
+        """
+        for layer in layers:
+            if isinstance(layer, QgsRasterLayer):
+                self.rlayers[layer.id()] = dict(name=layer.name(),
+                                                id=layer.id(),
+                                                obj=layer)
+                self.combox_raster.addItem(layer.name(), layer.id())
+                layer.layerNameChanged.connect(
+                    partial(self._layer_renamed, layer))
+                # TODO: wire
+            elif isinstance(layer, QgsVectorLayer):
+                self.vlayers[layer.id()] = dict(name=layer.name(),
+                                                id=layer.id(),
+                                                obj=layer)
+                self.combox_vector.addItem(layer.name(), layer.id())
+                # Wire rename
+                layer.layerNameChanged.connect(
+                    partial(self._layer_renamed, layer))
+                # Wire field change
+                layer.updatedFields.connect(
+                    partial(self._vlayer_modified, layer))
+                # TODO: Wire data changed
+
+    @QtCore.pyqtSlot(list)
+    def _map_layers_removed(self, layer_ids):
+        """ Keep track of removed layers
+
+        Remove from `self.rlayers` or `self.vlayers`
+        Remove QComboBox entry
+        Disconnect QgsMapLayer from signals
+
+        Args:
+          layer_ids (list): list of layer IDs that have been removed
+        """
+        logger.debug('Removing layers: {l}'.format(l=layer_ids))
+        # TODO: see docstring
+        for layer_id in layer_ids:
+            # Try to remove from both QComboxBox
+            idx = self.combox_raster.findData(layer_id)
+            self.combox_raster.removeItem(idx)
+            idx = self.combox_vector.findData(layer_id)
+            self.combox_vector.removeItem(idx)
+            # TOOD: if layer is to be deleted/removed, do we need to
+            #       disconnect it?
+
+    @QtCore.pyqtSlot(int)
+    def _vlayer_changed(self, idx):
+        """ Update when new vector layer has been selected
+
+        Args:
+          idx (int): index of `self.combox_vector` selected
+
+        """
+        logger.debug('Selected new index: {i}'.format(i=idx))
+
+        self.combox_field.clear()
+        if idx == -1:
+            return
+
+        # Determine layer
+        layer_id = self.combox_vector.itemData(
+            self.combox_vector.currentIndex())
+        layer = QgsMapLayerRegistry.instance().mapLayers()[layer_id]
+
+        n_features = layer.featureCount()
+        features = layer.getFeatures()
+        fields = layer.pendingFields()
+
+        # Update vector layer field name QComboBox
+        for field in fields:
+            self.combox_field.addItem(
+                '{n} ({t})'.format(n=field.name(), t=field.typeName()),
+                field.name())
+
+        # TODO: update attribute table
+        headers = ['FID'] + [f.name() for f in fields.toList()]
+        self.table_feature.clear()
+        self.table_feature.setColumnCount(len(headers))
+        self.table_feature.setRowCount(n_features)
+        self.table_feature.setHorizontalHeaderLabels(headers)
+        self.table_feature.horizontalHeader().setResizeMode(
+            QtGui.QHeaderView.Stretch)
+
+        for i, feat in enumerate(features):
+            item = QtGui.QTableWidgetItem()
+            item.setData(QtCore.Qt.DisplayRole, feat.id())
+            item.setTextAlignment(QtCore.Qt.AlignHCenter |
+                                  QtCore.Qt.AlignVCenter)
+            self.table_feature.setItem(i, 0, item)
+            for j, attr in enumerate(feat.attributes()):
+                item = QtGui.QTableWidgetItem()
+                item.setData(QtCore.Qt.DisplayRole, attr)
+                item.setTextAlignment(QtCore.Qt.AlignHCenter |
+                                      QtCore.Qt.AlignVCenter)
+                self.table_feature.setItem(i, j + 1, item)
+
+        self.table_feature.resizeColumnsToContents()
+
+    @QtCore.pyqtSlot(QgsVectorLayer)
+    def _vlayer_modified(self, vlayer):
+        """ Push an update when a vector layer has been modified
+
+        Note that this only takes action if `vlayer` modified is one currently
+        selected.
+
+        Args:
+          vlayer (QgsVectorLayer): vector layer modified
+
+        """
+        logger.debug('Vector layer {v} was modified'.format(v=vlayer.id()))
+        idx = self.combox_vector.findData(vlayer.id())
+        if idx == self.combox_vector.currentIndex():
+            self._vlayer_changed(idx)
+
+    @QtCore.pyqtSlot(QgsMapLayer)
+    def _layer_renamed(self, layer):
+        """ Keep track of renamed layers
+
+        """
+        logger.debug('Renamed layer {i}'.format(i=layer.id()))
+
+        if isinstance(layer, QgsRasterLayer):
+            idx = self.combox_raster.findData(layer.id())
+            self.combox_raster.setItemText(idx, layer.name())
+            self.rlayers[layer.id()]['name'] = layer.name()
+        elif isinstance(layer, QgsVectorLayer):
+            idx = self.combox_vector.findData(layer.id())
+            self.combox_vector.setItemText(idx, layer.name())
+            self.vlayers[layer.id()]['name'] = layer.name()
+
+    @QtCore.pyqtSlot()
+    def _update_plot(self):
+        """ Handle plot update request
+        """
+        logger.debug('ROI plot update requested')
+
+    @QtCore.pyqtSlot()
+    def _save_plot(self):
+        """ Handle a plot save request
+        """
+        logger.debug('ROI plot save requested')
+
+    @QtCore.pyqtSlot()
+    def _export_data(self):
+        """ Handle a data export request
+        """
+        logger.debug('ROI data export requested')
+
     def unload(self):
-        pass
+        QgsMapLayerRegistry.instance().layersAdded.disconnect(
+            self._map_layers_added)
+        QgsMapLayerRegistry.instance().layersWillBeRemoved.disconnect(
+            self._map_layers_removed)
+
+        self.but_update.clicked.disconnect(self._update_plot)
+        self.but_saveplot.clicked.disconnect(self._save_plot)
+        self.but_savestats.clicked.disconnect(self._export_data)
